911cba9ca9dbe0474eac3c2c51bdd93ad8edd077
デニス <dhenys.1002@gmail.com>
2021-02-13 13:32:16 -0500

    Create 01.addslashes.php
    Penjelasan !
    Garis miring ("\") akan ada sesuai koma atas,
    jika koma atas tidak ada, ya tidak ada garis miring ("\")

---
+++ php/001.Macam-Macam-Fungsi-String/01.addslashes.php
@@ -0,0 +1,8 @@
+<?php
+$text = "Sekarang hari jum'at";
+echo addslashes ($text);
+//Hasilnya : Sekarang hari jum\'at
+# Penjelasan 
+# Garis miring ("\") akan ada sesuai koma atas,
+# jika koma atas tidak ada, ya tidak ada garis miring ("\") 
+?>
