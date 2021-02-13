e97c2d84d4c18eb363863f1f164839719ffb6e48
デニス <dhenys.1002@gmail.com>
2021-02-14 04:02:56 +0700

    Create 02.chr.php
    Update ^.^

---
+++ php/001.Macam-Macam-Fungsi-String/02.chr.php
@@ -0,0 +1,8 @@
+<?php
+$kode = 81;
+$str = chr($kode);
+echo "karakter dengan kode ascii $kode adalah $str";
+// Hasilnya : karakter dengan kode ascii 81 adalah Q
+// untuk melihat kode ascii kunjungi link ini https://www.ascii-code.com/ 
+// kode DEC
+?>
