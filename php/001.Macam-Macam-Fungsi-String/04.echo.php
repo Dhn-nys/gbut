33010852d6a2869cea053811786ab21b12df5b03
デニス <dhenys.1002@gmail.com>
2021-02-17 04:37:42 +0700

    Create 04.echo.php
    ^.^

---
+++ php/001.Macam-Macam-Fungsi-String/04.echo.php
@@ -0,0 +1,7 @@
+<?php
+$Data1 ="Provinsi Kalimantan Barat";
+$Data2 ="Kota Pontianak";
+echo ("$Data1");
+//Hasilnya : Provinsi Kalimantan Barat
+# Hasilnya akan menampilkan hasil apa yang ada di string
+?>
